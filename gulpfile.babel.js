f53c19b30d675db8ff69c0e6fce251d2d5e878f4
stalinkay <stalin@stalinkay.com>
2016-05-26 02:45:43 +0100

    Fix Error: Cannot find module 'gulpLoadPlugins'
    gulpLoadPlugins is a typo should be gulp-load-plugins

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,9 +1,44 @@
 'use strict';
 
+// import plugins
 import gulp from 'gulp';
-import gulpLoadPlugins from gulpLoadPlugins;
+import gulpLoadPlugins from 'gulp-load-plugins';
+import browserSync from 'browser-sync';
 
+// constants
 const $ = gulpLoadPlugins();
+const reload = browserSync.reload;
+
+
+
+gulp.task('serve:dist', () => {
+  browserSync({
+    notify: false,
+    port: 9000,
+    server: {
+      baseDir: ['dist']
+    }
+  });
+});
+
+gulp.task('serve:test', ['scripts'], () => {
+  browserSync({
+    notify: false,
+    port: 9000,
+    ui: false,
+    server: {
+      baseDir: 'test',
+      routes: {
+        '/scripts': '.tmp/scripts',
+        '/bower_components': 'src/components'
+      }
+    }
+  });
+
+  gulp.watch('app/scripts/**/*.js', ['scripts']);
+  gulp.watch('test/spec/**/*.js').on('change', reload);
+  gulp.watch('test/spec/**/*.js', ['lint:test']);
+});
 
 /*
  *  This is the build task. It is runs all tasks
