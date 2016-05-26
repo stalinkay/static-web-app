0639d66d6fbf4c0c5391b0c0163e930289a7f087
stalinkay <stalin@stalinkay.com>
2016-05-26 14:59:24 +0100

    Add paths to assets
    dist directory should be deleted and created with every build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,6 +4,7 @@
 import gulp from 'gulp';
 import gulpLoadPlugins from 'gulp-load-plugins';
 import browserSync from 'browser-sync';
+import mkdirp from 'mkdirp';
 
 // constants
 const $ = gulpLoadPlugins();
@@ -16,6 +17,12 @@ const dirs = {
 };
 
 // sass paths
+const fontPaths = {
+  src: `${dirs.src}/fonts/`,
+  dest: `${dirs.dest}/fonts/`
+};
+
+// sass paths
 const sassPaths = {
   src: `${dirs.src}/sass/`,
   dest: `${dirs.dest}/css/`
@@ -24,7 +31,7 @@ const sassPaths = {
 // less paths
 const lessPaths = {
   src: `${dirs.src}/less/`,
-  dest: `${dirs.dest}/less/`
+  dest: `${dirs.dest}/css/`
 };
 
 // js paths
@@ -39,6 +46,64 @@ const imagePaths = {
   dest: `${dirs.dest}/images/`
 };
 
+/*
+ *  create required directories
+ */
+gulp.task('mkdir', () => {
+
+  let paths = {
+    sass: sassPaths,
+    less: lessPaths,
+    js: jsPaths,
+    images: imagePaths
+  };
+
+  mkdirp(dirs.dest, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('pow!');
+      }
+  });
+
+  mkdirp(sassPaths.src, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('pow!');
+      }
+  });
+
+  mkdirp(lessPaths.src, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('pow!');
+      }
+  });
+
+  mkdirp(jsPaths.src, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('pow!');
+      }
+  });
+
+  mkdirp(imagePaths.src, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('pow!');
+      }
+  });
+
+
+  // return gulp.src(dirs.src)
+  // .pipe($.util.noop())
+  // .pipe(gulp.dest(dirs.dest));
+
+});
 
 /*
  *  BrowseSync handles the loading, refreshing and synching between different browsers
@@ -88,13 +153,19 @@ gulp.task('changelog', () => {
 
 });
 
+/*
+ *  Create directories and cleanup
+ */
+gulp.task('setup', ['mkdir'], () => {
+  // return gulp.src('dist/**/*').pipe(gulp.dest(dirs.dist));
+});
 
 /*
  *  This is the build task. It is runs all tasks
  */
 //['lint', 'html', 'images', 'fonts', 'extras']
-gulp.task('build', ['changelog'], () => {
-  // return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
+gulp.task('build', ['changelog', 'setup'], () => {
+  return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
 });
 
 /*
